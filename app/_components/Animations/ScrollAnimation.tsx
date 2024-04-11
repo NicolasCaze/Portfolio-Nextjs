ca520da6d344921282c3139eda63e6b1a402d480
Nicolas <cazenicolas64@gmail.com>
2024-04-11 14:27:25 +0200

    correction bugs hebergement

--- app/_components/Animations/ScrollAnimation.tsx
+++ app/_components/Animations/ScrollAnimation.tsx
@@ -1,6 +1,11 @@
 import { motion, useViewportScroll, useTransform } from "framer-motion";
 
-const ScrollAnimation = ({ children, y }) => {
+interface ScrollAnimationProps {
+  children: React.ReactNode;
+  y: number;
+}
+
+const ScrollAnimation = ({ children, y }: ScrollAnimationProps) => {
   const { scrollYProgress } = useViewportScroll();
   const yRange = useTransform(scrollYProgress, [0, 1], [0, y]);
 
